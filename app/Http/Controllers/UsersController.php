a8227d8b58c58cde44410525ff4554ce3606252d
Rodrigo Póvoa <rpovoa29@gmail.com>
2023-11-22 00:06:04 +0000

    ok

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -11,23 +11,16 @@ class UsersController extends Controller
 {   
     public function search(Request $request)
     {
-
     $query = $request->input('q');
-    $sortBy = $request->input('sortBy');
 
+    // If the query is empty, return all users
     if (strlen($query) == 0) {
         $results = User::all();
     } else {
+        // Use where() with the 'like' operator to search usernames containing the query string
         $results = User::where('username', 'ilike', "%$query%")->get();
     }
 
-    if($sortBy == 'name'){
-        $results = $results->orderBy('name', 'desc');
-    } else if($sortBy == 'points'){
-        $results = $results->orderBy('points', 'desc');
-    } else if($sortBy == 'username'){
-        $results = $results->orderBy('', 'desc');
-    }
     return response()->json($results);
     }
 
