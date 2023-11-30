308fc40c2d33cfddce6e2049d059bf318cf7d8a6
Rodrigo Póvoa <up202108890@up.pt>
2023-11-30 09:47:17 +0000

    profilepage reworked

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -1,8 +1,7 @@
 @extends('layouts.barebone')
 
-@section('content2')
-
-    <style>
+@section('style')
+<style>
         #Profile{
             background-color: #0000FF;
             
@@ -10,7 +9,10 @@
         #Profile > a{
             color:white;
         } 
-    </style>
+</style>
+@endsection
+
+@section('content2')
     <nav>
         <a id='arrow' href="{{'/users'}}" >&larr;</a>
     </nav>
@@ -73,10 +75,4 @@
 <div id='Follow'><a class='aside' href="{{ route('followquestion', ['id' => $user->id]) }}">Followed Questions</a></div>
 <div id='MyQuestions'><a class='aside' href="{{ route('myquestions', ['id' => $user->id]) }}" >My questions</a></div>
 <div id= 'MyAnswers'><a class='aside' href="{{ route('myanswers', ['id' => $user->id]) }}">My answers</a></div>
-<div id='additional'>
-    <div>FAQ</div>
-    <div>About us</div>
-    <div>Contact us</div>
-    <div>Terms of service</div>
-</div>
 @endsection
--- resources/views/pages/userprofile.blade.php
+++ resources/views/pages/userprofile.blade.php
@@ -1,34 +1,11 @@
-<!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <!-- CSRF Token -->
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-
-        <title>{{ config('app.name', 'Laravel') }}</title>
-
-        <!-- Styles -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        <link href={{ url('css/editprofile.css') }} rel="stylesheet">
-        <script type="text/javascript">
-            // Fix for Firefox autofocus CSS bug
-            // See: http://stackoverflow.com/questions/18943276/html-5-autofocus-messes-up-css-loading/18945951#18945951
-        </script>
-        <script type="text/javascript" src={{ url('js/app.js') }} defer></script>
-    </head>
-    <body>
-            <header>
-                <h1><a href="{{ url('/home') }}">QthenA</a></h1>
-                @if (Auth::check())
-                     <a class="button" href="{{ route('profile', ['id' => $user->id]) }}">Go back</a>
-                @endif
-            </header>
+@section('style')
+    <link href="{{ url('css/editprofile.css') }}" rel="stylesheet">
+@endsection
+@section('content')
             <div class="container-xl px-4 mt-4">
             <!-- Account page navigation-->
-            <hr class="mt-0 mb-4">
             <div class="row">
                 <div class="col-xl-4">
                     <!-- Profile picture card-->
@@ -141,6 +118,5 @@
                     </div>
                 </div>
             </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    </body>
-</html> 
+        
+@endsection
