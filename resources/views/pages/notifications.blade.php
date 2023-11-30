8ae60550bf66bd5e923f8ac9326cca560833ce88
Tomás Sarmento <up202108778@fe.up.pt>
2023-11-30 10:35:35 +0000

    ok

--- resources/views/pages/notifications.blade.php
+++ resources/views/pages/notifications.blade.php
@@ -11,7 +11,6 @@
     <a class="active" href="{{'/questions'}}">Questions</a>
     <a href="{{'/users'}}">Users</a>
     </div>
-
     <div class="container">
     <div class="row">
         <div class="col-lg-9 right">
@@ -19,51 +18,21 @@
                 <div class="box-title border-bottom p-3">
                     <h6 class="m-0">Recent</h6>
                 </div>
+                @foreach ($notifications as $notification)
+                    <li>{{ $notification->notification_type }}</li>
+                    @if ($notification->notification_type == 'Answer')
+                        @break
+                    elseif ($notification->notification_type == 'Comment')
+
+                    elseif ($notification->notification_type == 'Badge')
+
+                    @endif
+                    @if ($notification->viewed)
+                        @break
+                    @endif
+                @endforeach
                 <div class="box-body p-0">
-                    <div class="p-3 d-flex align-items-center bg-light border-bottom osahan-post-header">
-                        <div class="dropdown-list-image mr-3">
-                            <img class="rounded-circle" src="https://ontent/avatar/avatar3.png" alt="" />
-                        </div>
-                        <div class="font-weight-bold mr-3">
-                            <div class="text-truncate">DAILY RUNDOWN: WEDNESDAY</div>
-                            <div class="small">Income tax sops on the cards, The bias in VC funding, and other top news for you</div>
-                        </div>
-                        <span class="ml-auto mb-auto">
-                            <div class="btn-group">
-                                <button type="button" class="btn btn-light btn-sm rounded" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    <i class="mdi mdi-dots-vertical"></i>
-                                </button>
-                                <div class="dropdown-menu dropdown-menu-right">
-                                    <button class="dropdown-item" type="button"><i class="mdi mdi-delete"></i> Delete</button>
-                                    <button class="dropdown-item" type="button"><i class="mdi mdi-close"></i> Turn Off</button>
-                                </div>
-                            </div>
-                            <br />
-                            <div class="text-right text-muted pt-1">3d</div>
-                        </span>
-                    </div>
-                    <div class="p-3 d-flex align-items-center osahan-post-header">
-                        <div class="dropdown-list-image mr-3">
-                            <img class="rounded-circle" src="https://ar" alt="" />
-                        </div>
-                        <div class="font-weight-bold mr-3">
-                            <div class="mb-2">We found a job at askbootstrap Ltd that you may be interested in Vivamus imperdiet venenatis est...</div>
-                            <button type="button" class="btn btn-outline-success btn-sm">View Jobs</button>
-                        </div>
-                        <span class="ml-auto mb-auto">
-                            <div class="btn-group">
-                                <button type="button" class="btn btn-light btn-sm rounded" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    <i class="mdi mdi-dots-vertical"></i>
-                                </button>
-                                <div class="dropdown-menu dropdown-menu-right">
-                                    <button class="dropdown-item" type="button"><i class="mdi mdi-delete"></i> Delete</button>
-                                    <button class="dropdown-item" type="button"><i class="mdi mdi-close"></i> Turn Off</button>
-                                </div>
-                            </div>
-                            <br />
-                            <div class="text-right text-muted pt-1">4d</div>
-                        </span>
-                    </div>
+
                 </div>
             </div>
             <div class="box shadow-sm rounded bg-white mb-3">
@@ -248,8 +217,9 @@
         </div>
     </div>
 </div>
-
+    
     <?php /*
+    @include('partials.question', ['question' => $question])
     <div style="color:white; font-size:0.0001em;">Home</div>
     <section id="notifications">
         @each('partials.notification', $notifications, 'notification')
