b54b90697a8e31b79c1bc8edc822dd23d59efb4c
Rodrigo Póvoa <rpovoa29@gmail.com>
2023-12-17 22:23:19 +0000

    Forgot password css fixedx

--- resources/views/auth/reset.blade.php
+++ resources/views/auth/reset.blade.php
@@ -7,11 +7,11 @@
 <p style="margin-top:6em">
     Hello {{ $user->name }}
 </p>
-<form method="POST" action="{{ route('resetpassword') }}">
+<form class="form" method="POST" action="{{ route('resetpassword') }}">
     @csrf
 
     <label for="password">New Password</label>
-    <input id="password" type="password" name="password" required>
+    <input id="password" type="password" name="password" placeholder="Enter Your New Password" required>
     @if ($errors->has('password'))
       <span class="error">
           {{ $errors->first('password') }}
@@ -19,9 +19,9 @@
     @endif
 
     <label for="password-confirm">Confirm New Password</label>
-    <input id="password-confirm" type="password" name="password_confirmation" required>
+    <input id="password-confirm" type="password" name="password_confirmation" placeholder="Enter Your New Password Again" required>
     <input type="hidden" name="token" value="{{ $user->remember_token }}">
-    <button type="submit">
+    <button class="button-submit" type="submit">
       Confirm Changes
     </button>
 </form>
