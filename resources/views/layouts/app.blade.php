ca1cb7db395563ced5eabc2956045e5242ffa8ea
Tomás Sarmento <up202108778@fe.up.pt>
2023-12-21 22:25:46 +0000

    added fulltext search

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -68,7 +68,7 @@
                         <li class="nav-item"><a href="/about" class="nav-link px-2 text-muted">Contacts</a></li>
                         <li class="nav-item"><a href="/about" class="nav-link px-2 text-muted">About Us</a></li>
                     </ul>
-                    <p class="text-center text-muted">© 2023 QthenA, Inc</p>
+                    <p class="text-center text-muted">© 2023 QthenA, In</p>
                 </footer>
             </div>
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
