15308204da25770dd2c84f0f8b3555c8cdae5492
justauser <diogosarmento4@gmail.com>
2023-12-21 17:47:05 +0000

    added css to the email

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -3,14 +3,21 @@
   <link href="{{ url('css/auth.css') }}" rel="stylesheet">
 @endsection
 @section('content')
-
+<div style ="padding:1em;">
 <p style= "display: flex;
     justify-content: space-evenly;">
     Hello {{ $user->name }}
 </p>
-<p>
+<p style= "display: flex;
+    justify-content: space-evenly;">
     You have requested to reset your password. Please click the link below to reset your password.
 </p>
-<a class="button button-primary" href="{{ route('reset', ['token' => $user->remember_token]) }}">Reset Password</a>
+
+  <a sytle="display: flex;
+    color: white !important;
+    width: 20em;
+    justify-content: space-evenly;
+    align-items: center;" class="button button-primary" href="{{ route('reset', ['token' => $user->remember_token]) }}">Reset Password</a>
+</div>
 
 @endsection
